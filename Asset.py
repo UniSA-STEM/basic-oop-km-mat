baffef48455b171fcb1bc200a3c2714b24b49580
Karl Matillano <matky024@mymail.unisa.edu.au>
2025-10-21 00:34:35 +1100

    commit with initial changes

--- Asset.py
+++ Asset.py
@@ -6,13 +6,21 @@ ID: 110336447
 Username: matky024
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+ASSET_LIBRARY = [ #define library in a list
+    ("CryptoToken", "Used to acquire or repair rigs"),
+    ("Data Spike", "Used in battles"),
+    ("Removable Drive", "Used for extraction"),
+    ("Security Chip", "Used to encrypt/decrypt assets"),
+    ("Hardware Patch", "Used to upgrade rigs")
+]
+
 class Asset:
 
     def __init__(self, name, description, encrypted = False): #By default, assets are not encrypted.
         self.name = name
         self.encrypted = encrypted #
         self.description = ("Unknown") #initiate description as unknown then
-        for n, desc in ASSET_LIBRARY:  #loop through asset list to fetch description from tuple
+        for n, desc in ASSET_LIBRARY:  #loop through asset list to fetch description from tuple  
             if n == name:
                 self.description = desc
                 break
--- Rig.py
+++ Rig.py
@@ -6,10 +6,17 @@ ID: 110336447
 Username: matky024
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+import random
+
+from Asset import ASSET_LIBRARY, Asset
+
+
 class Rig:
     def __init__(self, rig_name):
         self.rig_name = rig_name
         self.damage_counter = 0 #start at zero damage counter
+        self.durability = 2 #start at 2 for level 0 rig
         self.broken = False #start not broken
         self.storage = [ #starting asset of a rig
             Asset("Data Spike"),
@@ -18,17 +25,77 @@ class Rig:
         self.upgrade_level = 0  #start at 0 upgrade level
         self.max_storage_size = 3 #base capacity at level 0
 
-    def max_storage_size(self):     #increase max capacity along with upgrade level
-        return self.max_storage_size + self.upgrade_level
+    def rigrepair(self):
+        if self.damage_counter == 0: #check if there is damage to begin with.
+            print("No damage detected") #does not proceed with repair if rig has no damage
+            return
+        if hacker.cryptotoken < 1: #check if hacker has cryptotoken to use as currency for repair
+            print(f"{hacker.name} Not Enough Cryptotoken to repair {self.rig_name}. Repair is Aborted.")
+            return
+        hacker.cryptotoken -= 1 #removes one cryptotoken for repair
+        self.damage_counter = 0 #resets damage counter to 0 upon repair
+        self.broken = False     #remove broken status
+        print(f"Cryptotoken consumed for repair..."
+              f"{self.rig_name} is now fully repaired!")
+
+    def storage_size(self):   #returns storage size
+        return len(self.storage)
+
+    def max_capacity(self):     #increase max capacity along with upgrade level
+        return self.max_storage_size + self.upgrade_level * 2
+
+    def max_durability(self):       #increase max durability along with upgrade level
+        return self.durability + self.upgrade_level
 
     def upgrade(self):  #upgrade the rig
+        hardwarepatch = None
+        for stuff in self.storage: #check storage if it has a hardware patch
+            if stuff.name == "Hardware Patch": hardwarepatch = stuff
+            break
+
+        if not hardwarepatch:   #if no hardware patch found, send no patch detected message
+            print("No hardware patch detected")
+            return
+
+        self.storage.remove(stuff) #if patch found, remove one hardwarepatch
         self.upgrade_level += 1 #by one
-        print(f"{self.rig_name} upgraded to {self.upgrade_level} level! Capacity upgraded to {self.max_storage_size}!")
+
+        #show updated new stats upon upgrade
+        print(f"{self.rig_name} upgraded to Level {self.upgrade_level}!")
+        print(f"→ Max Storage increased to {self.max_storage_size} items.")
+        print(f"→ Max Durability increased to {self.durability} damage points.")
 
 
     def take_damage(self):
-        self.damage_counter += 1
+        self.damage_counter += 1    #each hit increases damage by 1
+
+        # assess broken status
+        if self.damage_counter >= self.durability:  #broken if damage counter exceeds durability
+            self.broken = True
+            print(f"{self.rig_name} is now broken!")
+        else:
+            print(f"{self.rig_name} has taken 1 damage."
+                  f"(HP left: {self.durability} durability left!")
 
     def condition(self):
-        condi = "Broken" if self.broken else "Pristine"
+        condi = "Broken" if self.broken else "Pristine" #
         return f"{condi} {self.damage_counter} damage (Level {self.upgrade_level})"
+
+    def generate_asset(self):
+        if self.storage_size() >= self.max_capacity():
+            print(f"{self.rig_name}'s storage capacity is full")
+            return
+        # fetch a random tuple in library as generated asset
+        name, description = random.choice(ASSET_LIBRARY)
+        newasset = Asset(name)
+        # show name and description of generated asset
+        print(f"{self.rig_name} generated {newasset.name} ({newasset.description})!")
+
+    def store_asset(self, h_asset):
+        #check if storage size is full
+        if self.storage_size() >= self.max_capacity():
+            print(f"{self.rig_name}'s storage capacity is full")
+            return
+        #add new asset to storage
+        self.storage.append(h_asset)
+        print(f"{self.rig_name} stored {h_asset.name}")
